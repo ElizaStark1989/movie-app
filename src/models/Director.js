da4a6b25e1180af624eb759d270219cd2cebfb68
Elizabeth <elizastark-1989@outlook.es>
2024-05-23 00:56:53 -0500

    backend de una app de peliculas

---
+++ src/models/Director.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Director = sequelize.define('director', {
+    firstName: {
+        type: DataTypes.STRING,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+    },
+    nationality: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+});
+
+module.exports = Director;
